07164002213404448fc1265769cb8f4023fe264f
korayerdem89 <developerkoray@gmail.com>
2022-08-14 22:01:00 +0300

    firebase ve dotenv eklendi

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+import Constants from 'expo-constants';
+
+export const firebaseConfig = {
+    apiKey: Constants.manifest?.extra?.firebaseApiKey,
+    authDomain: Constants.manifest?.extra?.firebaseAuthDomain,
+    projectId: Constants.manifest?.extra?.firebaseProjectId,
+    storageBucket: Constants.manifest?.extra?.firebaseStorageBucket,
+    messagingSenderId: Constants.manifest?.extra?.firebaseMessagingSenderId,
+    appId: Constants.manifest?.extra?.firebaseAppId,
+    databaseURL: Constants.manifest?.extra?.firebaseDatabaseURL,
+};
