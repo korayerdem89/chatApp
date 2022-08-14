235d687ee6eefd74dd755b4eac1588a249e771f0
korayerdem89 <developerkoray@gmail.com>
2022-08-14 16:41:45 +0300

    formik yapısı eklendi

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,22 +1,69 @@
 import React from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { ScrollView, View, Text, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import styles from './Auth.style';
 import Input from '../components/Input';
 import Button from '../components/Button';
+import { Formik } from "formik";
+import * as yup from "yup";
+
+const initialFormValues = {
+    username: "",
+    password: "",
+    repassword: "",
+};
+
+function handleSubmit() {
+null
+};
+
 const Auth = () => {
+    const SignupValidationSchema = yup.object().shape({
+        username: yup
+            .string()
+            .min(2, "Çok Kısa!")
+            .max(50, "Çok Uzun!")
+            .required(" "),
+    });
     return (
-        <View style={styles.container}>
-            <Text style={styles.header}>Chat App</Text>
-            <View style={styles.input_container}>
-            <Input  placeholder={"e-postanızı giriniz.."} />
-            <Input placeholder={"şifrenizi giriniz..."} />
-            <Input placeholder={"şifrenizi tekrar giriniz..."} />
-            </View>
-        <View style={styles.button_container}>
-         <Button isSignup text={"Kayıt Ol"} />
-         <Button text= {"Geri"}/>
-        </View>
-        </View>
+        <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={{ flex: 1 }}
+            keyboardVerticalOffset={-300}
+        >
+            <ScrollView style={styles.container}>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <>
+                        <Text style={styles.header}>Chat App</Text>
+                        <Formik
+            initialValues={initialFormValues}
+            onSubmit={handleSubmit}
+            validationSchema={SignupValidationSchema}
+          >
+            {({
+              handleSubmit,
+              handleChange,
+              values,
+              errors,
+              isValid,
+              dirty,
+            }) => (
+                        <View style={styles.form_container}>
+                            <View style={styles.input_container}>
+                                <Input placeholder={"e-postanızı giriniz.."} />
+                                <Input placeholder={"şifrenizi giriniz..."} />
+                                <Input placeholder={"şifrenizi tekrar giriniz..."} />
+                            </View>
+                            <View style={styles.button_container}>
+                                <Button isSignup text={"Kayıt Ol"} />
+                                <Button text={"Geri"} />
+                            </View>
+                        </View> 
+                         )}
+                        </Formik>
+                    </>
+                </TouchableWithoutFeedback>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 };
 
