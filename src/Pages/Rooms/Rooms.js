779767169e437ef750a381ff330034ae33965c1d
korayerdem89 <developerkoray@gmail.com>
2022-09-14 21:48:26 +0300

    roomName, databaseye kaydedildi

--- src/Pages/Rooms/Rooms.js
+++ src/Pages/Rooms/Rooms.js
@@ -1,22 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, ScrollView, FlatList, Alert } from 'react-native';
 import styles from './Rooms.style';
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../../../config/keys";
 import RoomContainer from '../../components/RoomContainer/RoomContainer';
 import AddRoom from '../../components/ModalViews/AddRoom';
+import { getDatabase, ref, onValue, push } from "firebase/database";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+auth.languageCode = "tr";
+const db = getDatabase();
+
 const Rooms = () => {
     const [isModalVisible, setModalVisible] = useState(false);
     const [roomName, setRoomName] = useState("");
     const [roomItems, setRoomItems] = useState([]);
 
-    const handleAddRoom = () => {
+    const handleAddRoom = async () => {
         if (roomItems.indexOf(roomName) > -1) {
             return Alert.alert('Bu isimde oda zaten mevcut');
         } 
+        const reference = await ref(db, `Rooms`);
+        push(reference, {
+          roomName
+        });
         setRoomItems([...roomItems, roomName]);
         setRoomName("");
         setModalVisible(false);
     };
 
+ 
+    
     console.log(roomItems);
   const removeItem = (index) => {
     let itemsCopy = [...roomItems];
